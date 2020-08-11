4fc098f6a080b9dd31c72a0f433dfb4bc5d75741
Mihaly Barasz <klao@nilcons.com>
2020-08-11 17:34:33 +0200

    Add Nat/BigInteger example

--- agda/js/main.js
+++ agda/js/main.js
@@ -1,7 +1,10 @@
 "use strict";
 
 const Tree = require("./jAgda.Tree");
+const { BigInteger } = require("./biginteger");
 
 console.log(Tree);
 
 console.log(Tree.showTree(Tree.exTree));
+
+console.log(Tree.showTree(Tree.binTree(new BigInteger(3))));
