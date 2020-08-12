29e3f442d646847a763a6228d1e1714be684a500
Mihaly Barasz <klao@nilcons.com>
2020-08-12 18:29:02 +0200

    Idris2: manually constructed tree

--- idris2/main.js
+++ idris2/main.js
@@ -2,3 +2,5 @@
 
 console.log(Tree_showTree(Tree_exTree()));
 console.log(Tree_showTree(Tree_binTree(3n)));
+const leaf = {h: 0};
+console.log(Tree_showTree({h: 1, a1: {h: 1, a1: leaf, a2: leaf}, a2: leaf}));
